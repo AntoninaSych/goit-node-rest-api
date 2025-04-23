56347f168fa14284967447a30ca1844e2b34c233
Antonina Sych <antoninasych@gmail.com>
2025-04-23 22:49:17 +0100

    working

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.get("/current", auth, async (req, res) => {
+    const { email, subscription } = req.user;
+    res.status(200).json({ email, subscription });
+});
+router.post("/logout", auth, async (req, res, next) => {
+    try {
+        await req.user.update({ token: null });
+        res.status(204).send(); // No Content
+    } catch (err) {
+        next(err);
+    }
+});
 module.exports = router;
